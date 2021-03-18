85dceab06882e1f307e1e4b6000071b850d921fe
flogy <gygy1992@hotmail.com>
2021-03-18 22:50:08 +0100

    feat: migrated to tailwindcss 2.0.4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { css } from '@emotion/core';
 
 const preflight = css`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+:root {
+  -moz-tab-size: 4;
+  tab-size: 4;
+}
 html {
   line-height: 1.15; 
   -webkit-text-size-adjust: 100%; 
@@ -8,30 +17,25 @@ html {
 body {
   margin: 0;
 }
-main {
-  display: block;
-}
-h1 {
-  font-size: 2em;
-  margin: 0.67em 0;
+body {
+  font-family:
+		system-ui,
+		-apple-system, 
+		'Segoe UI',
+		Roboto,
+		Helvetica,
+		Arial,
+		sans-serif,
+		'Apple Color Emoji',
+		'Segoe UI Emoji';
 }
 hr {
-  box-sizing: content-box; 
   height: 0; 
-  overflow: visible; 
-}
-pre {
-  font-family: monospace, monospace; 
-  font-size: 1em; 
-}
-a {
-  background-color: transparent;
+  color: inherit; 
 }
 abbr[title] {
-  border-bottom: none; 
-  text-decoration: underline; 
   -webkit-text-decoration: underline dotted;
-          text-decoration: underline dotted; 
+          text-decoration: underline dotted;
 }
 b,
 strong {
@@ -39,8 +43,15 @@ strong {
 }
 code,
 kbd,
-samp {
-  font-family: monospace, monospace; 
+samp,
+pre {
+  font-family:
+		ui-monospace,
+		SFMono-Regular,
+		Consolas,
+		'Liberation Mono',
+		Menlo,
+		monospace; 
   font-size: 1em; 
 }
 small {
@@ -59,8 +70,9 @@ sub {
 sup {
   top: -0.5em;
 }
-img {
-  border-style: none;
+table {
+  text-indent: 0; 
+  border-color: inherit; 
 }
 button,
 input,
@@ -73,80 +85,48 @@ textarea {
   margin: 0; 
 }
 button,
-input { 
-  overflow: visible;
-}
-button,
 select { 
   text-transform: none;
 }
 button,
-[type="button"],
-[type="reset"],
-[type="submit"] {
+[type='button'],
+[type='reset'],
+[type='submit'] {
   -webkit-appearance: button;
 }
-button::-moz-focus-inner,
-[type="button"]::-moz-focus-inner,
-[type="reset"]::-moz-focus-inner,
-[type="submit"]::-moz-focus-inner {
+::-moz-focus-inner {
   border-style: none;
   padding: 0;
 }
-button:-moz-focusring,
-[type="button"]:-moz-focusring,
-[type="reset"]:-moz-focusring,
-[type="submit"]:-moz-focusring {
+:-moz-focusring {
   outline: 1px dotted ButtonText;
 }
-fieldset {
-  padding: 0.35em 0.75em 0.625em;
+:-moz-ui-invalid {
+  box-shadow: none;
 }
 legend {
-  box-sizing: border-box; 
-  color: inherit; 
-  display: table; 
-  max-width: 100%; 
-  padding: 0; 
-  white-space: normal; 
+  padding: 0;
 }
 progress {
   vertical-align: baseline;
 }
-textarea {
-  overflow: auto;
-}
-[type="checkbox"],
-[type="radio"] {
-  box-sizing: border-box; 
-  padding: 0; 
-}
-[type="number"]::-webkit-inner-spin-button,
-[type="number"]::-webkit-outer-spin-button {
+::-webkit-inner-spin-button,
+::-webkit-outer-spin-button {
   height: auto;
 }
-[type="search"] {
+[type='search'] {
   -webkit-appearance: textfield; 
   outline-offset: -2px; 
 }
-[type="search"]::-webkit-search-decoration {
+::-webkit-search-decoration {
   -webkit-appearance: none;
 }
 ::-webkit-file-upload-button {
   -webkit-appearance: button; 
   font: inherit; 
 }
-details {
-  display: block;
-}
 summary {
   display: list-item;
-}
-template {
-  display: none;
-}
-[hidden] {
-  display: none;
 }
 blockquote,
 dl,
@@ -182,8 +162,12 @@ ul {
   padding: 0;
 }
 html {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; 
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; 
   line-height: 1.5; 
+}
+body {
+  font-family: inherit;
+  line-height: inherit;
 }
 *,
 ::before,
@@ -191,7 +175,7 @@ html {
   box-sizing: border-box; 
   border-width: 0; 
   border-style: solid; 
-  border-color: #e2e8f0; 
+  border-color: #e5e7eb; 
 }
 hr {
   border-top-width: 1px;
@@ -202,15 +186,10 @@ img {
 textarea {
   resize: vertical;
 }
-input:-ms-input-placeholder, textarea:-ms-input-placeholder {
-  color: #a0aec0;
-}
-input::-ms-input-placeholder, textarea::-ms-input-placeholder {
-  color: #a0aec0;
-}
 input::placeholder,
 textarea::placeholder {
-  color: #a0aec0;
+  opacity: 1;
+  color: #9ca3af;
 }
 button,
 [role="button"] {
@@ -245,7 +224,7 @@ pre,
 code,
 kbd,
 samp {
-  font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
 }
 img,
 svg,
